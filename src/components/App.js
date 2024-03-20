2f46dc5ad77b6eb2273afd3ea4958679ae1a03e8
deboraviveiros <deviveiros.debora@gmail.com>
2024-03-20 11:29:06 -0300

    explicações moni

--- src/components/App.js
+++ src/components/App.js
@@ -8,3 +8,8 @@ const App = () => {
 };
 
 export default App;
+
+/* parecido com o view - ideia de componentes - um pedaço de interface - 
+aqui teremos que renderizar os filmes, usar o fetch - pegar os dados para printar na tela
+
+entendeu a estrutura, chama o endpoint pra renderizar. faz o teste unitário */
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import App from './components/App.js';
 
 document.getElementById('root').appendChild(App());
+
+
+/* pega a id root e dá um appendChild em App. o App é filho da root */
