f1416b8ff1ecd52adc067b235f41f1e37a85959c
deboraviveiros <deviveiros.debora@gmail.com>
2024-03-26 19:14:29 -0300

    estilização

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 const App = async () => {
   let ul = document.createElement('ul');
   document.body.appendChild(ul);
-
   const options = {
     method: 'GET',
     headers: {
@@ -12,7 +11,7 @@ const App = async () => {
 
   try {
     // Obtém a resposta da API do TMDB
-    const response = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=pt-BR&page=1&sort_by=popularity.desc', options);
+    const response = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=pt-BR&page=1&sort_by=popularity.desc&year=1987', options);
     // Extrai o JSON da resposta e guarda os dados em uma variável
     const data = await response.json();
     // Obtém apenas os filmes (results) da resposta e exibe os títulos e datas de lançamento
@@ -43,55 +42,4 @@ const App = async () => {
   }
 };
 
-// Chama a função App para iniciar a busca e exibição dos filmes
-App();
-
 export default App;
-
-
-/* parecido com o view - ideia de componentes - um pedaço de interface - 
-aqui teremos que renderizar os filmes, usar o fetch - pegar os dados para printar na tela
-
-entendeu a estrutura, chama o endpoint pra renderizar. faz o teste unitário 
-
-Fetch Request TMDB
-header da requisição
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZGZkNWQ0OWI4YzE0MmM2MjlmOTA2YzhiODliMjZkYiIsInN1YiI6IjY1ZjVhNjIxM2MzYWIwMDE3ZGQwMjc2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k66Yx9thwDS90B8OzFnrwz8EqkzRrf7od4SG1Ip7Dx4'
-  }
-};
-
-fetch('https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=pt-BR', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
-
-// Função async para buscar informações dos filmes
-
-
-// Executa a busca assim que o código for carregado
-fetchMovies(); chamar dentro do método principal
-
-// Cria um novo elemento HTML <div>. Você pode alterar o tipo de elemento se necessário.
-  const el = document.createElement('div');
-
-  // Define a classe CSS do elemento <div> como "App". Você pode alterar essa classe para estilizar o elemento de maneiras diferentes.
-  el.className = 'App';
-
-  // Define o texto dentro do elemento <div> como "Hola mundo!". Você pode alterar o texto conforme sua necessidade.
-  el.textContent = 'Hola mundo!';
-
-  // Retorna o elemento <div> configurado. Normalmente, você não precisa alterar essa linha.
-  return el;
-
-const movieElement = document.createElement('div');
-      movieElement.textContent = `${movie.title} (${movie.release_date})`;
-      document.body.appendChild(movieElement); // precisa retornar algo para usar no App.js. ao invés de dar append no body, dar append em uma div fora do for / montar o html e dar um return
-    
-
-
-*/
