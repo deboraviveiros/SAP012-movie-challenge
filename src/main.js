24dc0397054c4322929750e5d8406b1b0d757e7a
deboraviveiros <deviveiros.debora@gmail.com>
2024-03-27 09:45:42 -0300

    atualizações sprint review

--- src/main.js
+++ src/main.js
@@ -4,3 +4,15 @@ document.getElementById('root').appendChild(App());
 
 
 /* pega a id root e dá um appendChild em App. o App é filho da root */
+
+/* Para fazer:
+- Implementar paginação (5 filmes por página);
+- Construir detalhes dos filmes;
+- Construir testes unitários;
+- Corrigir estilização;
+- Fazer deploy;
+
+Se der tempo:
+- Trabalhar responsividade;
+- Implementar filtro e ordenação;
+*/
